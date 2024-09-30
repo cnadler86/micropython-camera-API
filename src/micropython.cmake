5b5fd11b25b0c91fc918ba337dde400e1efcba28
Christopher Nadler <147471517+cnadler86@users.noreply.github.com>
2024-09-30 06:26:21 +0200

    micropython.cmake aktualisieren

--- src/micropython.cmake
+++ src/micropython.cmake
@@ -1,3 +1,18 @@
+list(APPEND SRC_CAMERA
+	modcamera.c
+	modcamera_api.c
+)
+
+idf_component_register(
+    SRCS ${SRC_CAMERA}
+    INCLUDE_DIRS "."
+)
+
+if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/sdkconfig")
+    message(STATUS "Using custom sdkconfig")
+    set(SDKCONFIG_DEFAULTS "${CMAKE_CURRENT_SOURCE_DIR}/sdkconfig")
+endif()
+
 add_library(usermod_mp_camera INTERFACE)
 target_sources(usermod_mp_camera INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/modcamera.c
