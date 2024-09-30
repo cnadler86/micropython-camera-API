e5be1e441e7b6ea1367b1900be5afc5b881c7b21
Christopher Nadler <147471517+cnadler86@users.noreply.github.com>
2024-09-30 07:11:24 +0200

    micropython.cmake aktualisieren

--- src/micropython.cmake
+++ src/micropython.cmake
@@ -1,25 +1,15 @@
-list(APPEND SRC_CAMERA
-	modcamera.c
-	modcamera_api.c
-)
-
+# ESP-IDF-Komponentenregistrierung
 idf_component_register(
-    SRCS ${SRC_CAMERA}
+    SRCS modcamera.c modcamera_api.c
     INCLUDE_DIRS "."
 )
 
+# Überprüfe, ob sdkconfig vorhanden ist, um benutzerdefinierte Konfiguration zu verwenden
 if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/sdkconfig")
     message(STATUS "Using custom sdkconfig")
     set(SDKCONFIG_DEFAULTS "${CMAKE_CURRENT_SOURCE_DIR}/sdkconfig")
 endif()
 
-add_library(usermod_mp_camera INTERFACE)
-target_sources(usermod_mp_camera INTERFACE
-    ${CMAKE_CURRENT_LIST_DIR}/modcamera.c
-    ${CMAKE_CURRENT_LIST_DIR}/modcamera_api.c
-)
-target_include_directories(usermod_mp_camera INTERFACE
-    ${CMAKE_CURRENT_LIST_DIR}
-)
-target_compile_definitions(usermod_mp_camera INTERFACE)
-target_link_libraries(usermod INTERFACE usermod_mp_camera)
+# Micropython usermod Konfiguration
+target_sources(${COMPONENT_LIB} PRIVATE modcamera.c modcamera_api.c)
+target_include_directories(${COMPONENT_LIB} PRIVATE ${CMAKE_CURRENT_LIST_DIR})
