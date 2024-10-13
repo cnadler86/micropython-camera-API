43d47ed3cbaa81d335cf68c54796aa5fc8d51f52
Christopher Nadler <christopher.nadler@gmail.com>
2024-10-13 05:05:07 +0200

    This should fly

--- src/micropython.cmake
+++ src/micropython.cmake
@@ -1,8 +1,13 @@
+# `py.cmake` for `micropy_gather_target_properties` macro usage
+include(${MICROPY_DIR}/py/py.cmake)
+
 add_library(usermod_mp_camera INTERFACE)
+
 target_sources(usermod_mp_camera INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/modcamera.c
     ${CMAKE_CURRENT_LIST_DIR}/modcamera_api.c
 )
+
 target_include_directories(usermod_mp_camera INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
     ${IDF_PATH}/components/esp32-camera/driver/include
@@ -11,8 +16,10 @@ target_include_directories(usermod_mp_camera INTERFACE
     ${IDF_PATH}/components/esp32-camera/conversions/private_include
     ${IDF_PATH}/components/esp32-camera/sensors/private_include
 )
-target_compile_definitions(usermod_mp_camera INTERFACE)
-target_link_libraries(usermod INTERFACE usermod_mp_camera)
-if (MP_CAMERA_MODEL)
-	add_compile_definitions(MP_CAMERA_MODEL_${MP_CAMERA_MODEL}=1)
+
+if (MICROPY_CAMERA_MODEL)
+    target_compile_definitions(usermod_mp_camera INTERFACE MICROPY_CAMERA_MODEL_${MICROPY_CAMERA_MODEL}=1)
 endif()
+
+target_link_libraries(usermod INTERFACE usermod_mp_camera)
+micropy_gather_target_properties(usermod_mp_camera)
