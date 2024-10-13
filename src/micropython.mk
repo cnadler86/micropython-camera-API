cd0b2e05c8ed01b4fbcbce411cf3def7febef9b4
Christopher Nadler <147471517+cnadler86@users.noreply.github.com>
2024-10-13 05:45:24 +0200

    Add board build support to pipeline (#7)
    Pipeline can additionally specify camera board model in order to populate default pins.

--- src/micropython.mk
+++ src/micropython.mk
@@ -1,3 +1,4 @@
 CAMERA_MOD_DIR := $(USERMOD_DIR)
 SRC_USERMOD_C += $(addprefix $(CAMERA_MOD_DIR)/, modcamera_api.c)
 SRC_USERMOD_LIB_C += $(addprefix $(CAMERA_MOD_DIR)/, modcamera.c)
+CFLAGS_USERMOD += -I$(CAMERA_MOD_DIR)
